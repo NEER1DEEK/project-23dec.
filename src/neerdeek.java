39c4af45d6ecb7f07a0eccae264c65cecd6d2622
admin <neer1deek>
2021-12-23 15:03:20 +0530

    Initial commit-23dec.

---
+++ src/neerdeek.java
@@ -0,0 +1,7 @@
+public class neerdeek {
+
+       public static void main(String[]args){
+        System.out.println("i am not in the classroom");
+        System.out.println("i have notebok");
+    }
+}
